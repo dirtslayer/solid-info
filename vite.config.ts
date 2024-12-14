cdada47cb815f2e7d6f7a2c148efda1324871e36
Darrell D <dirtslayer@gmail.com>
2024-12-13 20:11:09 -0700

    deploy base to folder github project

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
+base: "https://dirtslayer.github.io/solid-info/",
 plugins: [solidPlugin()],
 build: {
   target: "esnext",
